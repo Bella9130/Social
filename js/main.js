7cdddbb883b76b1969f7f72c52900511b54b7847
Bella <salguero.cano.bellla@gmail.com>
2023-09-21 10:24:00 -0600

    Proyecto

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//--- Padre --- 
+//-- hijo -- 
+//-- no tan importate
+
+
+// --- Para llamar o pausar el audio del fondo ---
+const playAudioButton = document.getElementById('audio-img') 
+//-- Audio Pausar
+const pauseAudio = document.getElementById('')
